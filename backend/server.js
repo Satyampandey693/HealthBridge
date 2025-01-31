a7e1556f8bd1a922e08f4dedded18125bdc963f9
Satyampandey693 <satyampandey0903@gmail.com>
2025-01-31 22:25:02 +0530

    user Model created and backend set successfully

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express"
 import { fileURLToPath } from 'url';
 import path from "path"
+import dotenv from "dotenv";
+dotenv.config({ path: "./config/config.env" });
 const app = express()
 const port = process.env.PORT||3000
 
